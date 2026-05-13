a6af600d9d3a7dcf3b5659549c61edf45d7a3df8
Eddie <2456767505@qq.com>
2026-05-13 16:39:09 +0800

    本地调试代码。

---
+++ wlizhi-test/src/main/java/cc/wlizhi/wlizhitest/WlizhiTestApplication.java
@@ -0,0 +1,13 @@
+package cc.wlizhi.wlizhitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlizhiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WlizhiTestApplication.class, args);
+	}
+
+}
---
+++ wlizhi-test/src/test/java/cc/wlizhi/wlizhitest/WlizhiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.wlizhi.wlizhitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlizhiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
